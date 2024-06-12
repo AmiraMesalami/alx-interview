13695e15a82c8e894632f17db579402e034f6603
AmiraMesalami <amiraa.elmosallamy@gmail.com>
2024-06-12 07:04:13 +0000

    mero

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,18 @@
 #!/usr/bin/node
+
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2) {
-  const getCharacterName = (url) => {
-    return new Promise((resolve, reject) => {
-      request(url, (err, _, body) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(JSON.parse(body).name);
-        }
-      });
-    });
-  };
-
-  const filmId = process.argv[2];
-  const filmURL = `${API_URL}/films/${filmId}/`;
-
-  request(filmURL, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    } else {
-      const charactersURL = JSON.parse(body).characters;
-      const charactersNamePromises = charactersURL.map(getCharacterName);
-
-      Promise.all(charactersNamePromises)
-        .then(names => console.log(names.join('\n')))
-        .catch(err => console.log(err));
-    }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-}
+};
+
